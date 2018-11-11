9bef6fa25cdeb4d056d77a00d0a5639a0cda24db
jtiagopcosta <44501938+jtiagopcosta@users.noreply.github.com>
2018-11-11 21:15:15 +0000

    upload de imagem de utilizador

--- database/acao.php
+++ database/acao.php
@@ -8,6 +8,24 @@
    If ($conn)
    {
 
+       /*Upload de imagem para o servidor */
+       $target_dir = "../img/";
+       $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+       $uploadOk = 1;
+       $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+
+       /* diretório para a imagem guardada (este diretório é gravado na base de dados mais abaixo) */
+       $file = $_FILES['fileToUpload'];
+       $name = $file['name'];
+
+       $path = "../img/" . basename($name);
+       if (move_uploaded_file($file['tmp_name'], $path)) {
+           // Move succeed.
+       } else {
+           // Move failed. Possible duplicate?
+       }
+
         $nome = $_POST['nome'];        
         $username = $_POST['username'];
         $email = $_POST['email'];
@@ -21,8 +39,8 @@
             Atribui a variável $sql a instrução para inserir um registro.
             OBS.: Na instrução SQL estou supondo que exista no banco de dados a tabela nomes com as colunas id e nome.
        */
-        $sql = "INSERT INTO usuarios (nome, username, email, senha_md5)
-                VALUES ('$nome','$username','$email','".$password_md5."')";
+        $sql = "INSERT INTO usuarios (nome, username, email, senha_md5, imagem)
+                VALUES ('$nome','$username','$email','".$password_md5."', '$path')";
         $result = pg_exec($conn, $query);
 
        /**
