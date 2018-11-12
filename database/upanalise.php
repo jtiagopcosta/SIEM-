618e12c0d80ab5f75956e3e7c22e472f5bd30992
jtiagopcosta <44501938+jtiagopcosta@users.noreply.github.com>
2018-11-12 03:07:51 +0000

    f

--- database/upanalise.php
+++ database/upanalise.php
@@ -1,12 +1,15 @@
 <?php
 
 include_once ("../common/database.php");
-
-$analise = $_POST['analise'];
+session_start();
+$idfilme = $_GET['id'];
+$analise = $_POST['mensagem'];
+$id = $_SESSION['id'];
 
 $query = "set schema 'trabalho2';";	
 pg_exec($conn, $query);
 
-$query = "INSERT INTO analises (hora, analise ) VALUES ('$nome', '$analise')";
+$query = "INSERT INTO analises ( analise, idusuario, idfilme ) VALUES ( '$analise', '$id', '$idfilme')";
 pg_query($conn, $query);
+pg_close($conn); 
 ?>
