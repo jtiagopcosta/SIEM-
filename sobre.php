6a4edadb7cfc0c0fabf46ff7a4f5440ebd4948c5
jtiagopcosta <44501938+jtiagopcosta@users.noreply.github.com>
2018-11-12 05:25:46 +0000

    sobre

---
+++ sobre.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<link rel="stylesheet"  href="css/style.css" type="text/css"/>
+		<link rel="stylesheet"  href="css/sec.css" type="text/css"/>
+		<link rel="stylesheet"  href="css/login.css" type="text/css"/>
+		<link rel="stylesheet"  href="css/submit.css" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <body>
+        <!--DIVISORIA LOGIN-->
+		<div >
+                <?php
+                session_start();
+                if($_SESSION['usuario'] ){ ?>
+    
+                            
+                            <ul class="barra">	
+                                Olá <a  class="c" href="perfil.php?id=<?=$_SESSION['id']?>"> <b><?=$_SESSION['nome']?></b></a>, como está?             
+                                <form method='post' action='database/acaoLogout.php'>
+                                <input class="logout" type='submit' name='logout' value='logout'></input>
+                                </form>
+                            </ul>	
+    
+                <?php }
+                else if($_SESSION['administrador'] ){ ?>
+                                    
+                            
+                            <ul class="barra">	
+                                Olá <a  class="c" href="perfil.php?id=<?=$_SESSION['id']?>"> <b><?=$_SESSION['nome']?></b></a>, como está?             
+                                <form method='post' action='database/acaoLogout.php'>
+                                <input class="logout" type='submit' name='logout' value='logout'></input>
+                                </form>
+                            </ul>	
+    
+                <?php }
+    
+                else {?>
+                                <ul class="barra">
+                                <a href="registro.php" class="registro">Registar-se</a>
+                                <form method="POST" action="database/validacao_user.php">
+                                <input class="submitlogin" type="submit" value="Sign in" />
+                                <li class="login"><input type="Password" name="senha" placeholder="Password" class="firstbar"></li>
+                                <li class="login"><input type="Login" name="nome" placeholder="Username" class="firstbar"></li>	
+                                </form>
+                <?php } ?>
+            </div>
+    
+            <div id="div_top">
+                    <h1>Cin&eacutefilos.pt</h1>
+                    <img src="./img/title.jpg" width="100%" height="100%">
+            </div>
+    
+            <!-- menu -->
+            <div>
+                <ul>
+                    <?php
+                    session_start();
+                    if($_SESSION['administrador'] ){ ?>
+                            <li><a  href="index.php">Em destaque</a></li>
+                            <li><a  href="filmes.php">Filmes</a></li>
+                            <li><a href="formulario.php">Inserir</a></li>
+                            <form method="POST" action="filmespesquisados.php">
+                                <input type="search" name="pesquisa" placeholder="pesquisa" class="pesquisadorAdm">
+                                </form>
+                            
+                            <?php }
+                                    else {
+                                        ?>
+    
+                            <li><a  href="index.php">Em destaque</a></li>
+                            <li><a  href="filmes.php">Filmes</a></li>
+                            <li><a class="active" href="sobre.php">Sobre</a></li>
+                            <li  class="barrapesquisa">
+                                <form method="POST" action="filmespesquisados.php">
+                                <input type="search" name="pesquisa" placeholder="pesquisa" class="pesquisador">
+                                </form>
+                            </li>
+                    <?php } ?>
+                </ul>
+            </div>
+            
+   
+    </body>
+</html>
